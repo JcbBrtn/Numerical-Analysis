7951ac801d2cebe91bfe9a6ce926c0556841a96c
JcbBrtn <42615937+JcbBrtn@users.noreply.github.com>
2020-10-18 20:08:42 -0400

    added more algo division

--- One_Var_Equation_Solutions/Solving_One_Variable_Equations.py
+++ One_Var_Equation_Solutions/Solving_One_Variable_Equations.py
@@ -1,12 +1,10 @@
-#Some handy Algorithms to find Solutions of Equations of One Variable
-
 def muller_method(f, p0, p1, p2, tol=0.00000001, N=100):
     """
     This Algorithm first presented by D.E. Muller finds the solution to f(x)=0
     given three aproximates, p0, p1, p2.
     f is defined here as a lambda function, p0-p2 are its approximate solutions.
     Tolerance is the least change to our solution we are willing to have before return a value
-    n0 is the maximum number of iterations before we return failure.
+    N is the maximum number of iterations before we return failure.
     """
     #print('Muller\'s Method Called...')
     h1 = p1-p0
@@ -45,7 +43,11 @@ def muller_method(f, p0, p1, p2, tol=0.00000001, N=100):
     return 'failure'
 
 def secant_method(f, p0, p1, tol=0.00000001, N=100):
-    #find f(x)=0 given initial approximations p0 and p1
+    """
+    f is defined here as a lambda function, p0-p1 are its approximate solutions.
+    Tolerance is the least change to our solution we are willing to have before return a value
+    N is the maximum number of iterations before we return failure.
+    """
     #print('Secant Method Called...')
     i = 2
     q0 = f(p0)
@@ -64,8 +66,49 @@ def secant_method(f, p0, p1, tol=0.00000001, N=100):
         q1 = f(p)
     #if method failed to converge
     return 'failure'
-"""
-f = lambda x: x**4 - 3*x**3 + x**2 + x + 1
-print(muller_method(f, 0.5, 1.0, 1.5))
-print(secant_method(f, 0.5, 1.0))
-"""
+
+def bisection_method(f, a, b, tol=0.000001, N=100):
+    """
+    Method for finding the root of an equation of the form f(x)=0,
+    for a given lambda function f in the range of [a,b] where f(a) and f(b)
+    are opposite signs.
+    """
+    #f(a) and f(b) must have opposite signs for this method to work
+    if f(a) * f(b) > 0:
+        print('f(a) * f(b) > 0 Must have opposite signs for method to work.')
+        return 'failure'
+    i = 1
+    FA = f(a)
+    while i <= N:
+        p = a+(b-a)/2
+        FP = f(p)
+        if FP == 0 or (b-a)/2 < tol:
+            return p
+        i += i
+
+        if FA * FP > 0:
+            a = p
+            FA = FP
+        else:
+            b = p
+    print('Method failed after N iterations, N=',N)
+    return 'failure'
+
+def fixed_point_iteration(f, p0, tol=0.00000001, N=100):
+    """
+    Finds solution to to p = f(p) given an initial approximation of p0
+    """
+
+    i = 1
+
+    while i <= N:
+        p = f(p0)
+
+        if abs(p - p0) < tol:
+            return p
+
+        i+=1
+        p0 = p
+
+    print('Method failed after N iterations, N= ',N)
+    return 'failure'
